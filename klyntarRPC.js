12d2f2bae189aee6bcc2a103bd88a55d2bd16e80
VladChernenko <vladchernenko@protonmail.com>
2025-02-19 14:34:32 +0200

    Added websocket messages handler

--- klyntarRPC.js
+++ klyntarRPC.js
@@ -56,8 +56,8 @@ export let EVM_ROUTE_HANDLER = async(request,response) => {
 
     response.header('Access-Control-Allow-Origin','*')
 
-    //Body looks like this {"jsonrpc": "2.0", "method": "subtract", "params": [42, 23], "id": 1}
-    //Response looks like {"jsonrpc": "2.0", "result": 19, "id": 1}
+    // Body looks like this {"jsonrpc": "2.0", "method": "subtract", "params": [42, 23], "id": 1}
+    // Response looks like {"jsonrpc": "2.0", "result": 19, "id": 1}
 
     let body = request.body
 
@@ -78,6 +78,31 @@ export let EVM_ROUTE_HANDLER = async(request,response) => {
         }else response.send(ERROR_RETURN(-32601,"Method not found",body.id))
 
     }else response.send(ERROR_RETURN(-32600,"Invalid Request",body.id))
+
+}
+
+
+
+
+export let WEBSOCKET_EVM_ROUTE_HANDLER = async body => {
+
+    let paramsAreOk = body?.params ? Array.isArray(body.params) : true 
+
+    if(!body) return ERROR_RETURN(-32700,"Parse error",body.id)
+    
+    else if(body.jsonrpc==='2.0' && typeof body.method==='string' && paramsAreOk){
+
+        if(METHODS_MAPPING.has(body.method)){
+
+            let result = await METHODS_MAPPING.get(body.method)(body.params)
+
+            if(result.error) return ERROR_RETURN(-32602,"Invalid params => "+result.error,body.id)
+
+            else return RETURN_RESULT(result,body.id)
+
+        }else return ERROR_RETURN(-32601,"Method not found",body.id)
+
+    }else return ERROR_RETURN(-32600,"Invalid Request",body.id)
 
 
 }
